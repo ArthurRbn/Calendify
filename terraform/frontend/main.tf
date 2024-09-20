f5676474c6d34fd175905de2421f9883e118a030
Arthur ROBINE <arthur.robine@gmail.com>
2024-09-20 21:52:39 +0200

    fix frontend module variables

--- terraform/frontend/main.tf
+++ terraform/frontend/main.tf
@@ -5,7 +5,5 @@ module "frontend_s3" {
 module "frontend_cloudfront" {
   source           = "../common"
   s3_bucket_name   = module.frontend_s3.s3_bucket_name
-  s3_bucket_website_url = module.frontend_s3.s3_bucket_website_url
   frontend_domain  = var.frontend_domain
-  acm_certificate_arn = module.frontend_s3.acm_certificate_arn
 }
