35fcfeabc995d7631add13e22ad958e1d11c9e69
Arthur ROBINE <arthur.robine@gmail.com>
2024-09-20 20:46:05 +0200

    remove var usage for the backend region as it is unusable here and use REGION env variables automatically

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-calendify"
     key            = "calendify/terraform.tfstate"
-    region         = var.region
     dynamodb_table = "terraform-locks-calendify"
   }
 }
