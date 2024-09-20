1fc8d8f130d91f3ef51ee044c07621ebd0c28ab2
Arthur ROBINE <arthur.robine@gmail.com>
2024-09-20 21:06:55 +0200

    fix acm cert arn variable

--- terraform/common/variables.tf
+++ terraform/common/variables.tf
@@ -10,11 +10,6 @@ variable "frontend_domain" {
   default = "www.calendify.arthurrobine.fr"
 }
 
-variable "acm_certificate_arn" {
-  description = "ACM certificate ARN for CloudFront"
-  type        = string
-}
-
 variable "s3_bucket_name" {
   description = "The name of the S3 bucket for the frontend"
   default     = "calendify-frontend-bucket"
