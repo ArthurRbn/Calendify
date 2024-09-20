c95c6f489589ac10d7e7baa456c1fe2acc00aac7
Arthur ROBINE <arthur.robine@gmail.com>
2024-09-20 21:54:26 +0200

    fix root main variables

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,9 +19,6 @@ module "common" {
 # Frontend (S3 + CloudFront setup)
 module "frontend" {
   source = "./frontend"
-  s3_bucket_name       = module.common.s3_bucket_name
   frontend_domain      = var.frontend_domain
-  acm_certificate_arn  = module.common.acm_certificate_arn
-  s3_bucket_website_url = module.common.s3_bucket_website_url
 }
 
