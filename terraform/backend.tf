c19c970e1b4c574120624068b1a04bfe86705d93
Arthur ROBINE <arthur.robine@gmail.com>
2024-09-21 03:35:07 +0200

    add backend region

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-calendify"
     key            = "state/terraform.tfstate"
+    region         = "eu-west-3"
     dynamodb_table = "terraform-locks-calendify"
   }
 }
