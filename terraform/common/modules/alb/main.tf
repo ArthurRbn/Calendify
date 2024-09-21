d1c3cacd8d87ba83cfa99d00486199279e2251ae
Arthur ROBINE <arthur.robine@gmail.com>
2024-09-21 21:31:07 +0200

    remove default value

--- terraform/common/modules/alb/main.tf
+++ terraform/common/modules/alb/main.tf
@@ -29,7 +29,6 @@ resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.this.arn
   port              = 443
   protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = var.acm_certificate_arn
 
   default_action {
